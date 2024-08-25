f0c9bac5112a4f260b5db9f61d4fdf8b79641817
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-08-25 19:26:10 +0530

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Sajed Shaikh on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
